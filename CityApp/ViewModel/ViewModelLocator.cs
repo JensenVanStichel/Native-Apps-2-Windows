067b8c5ac3d9676fef79c1a80609be0a2856b76b
JensenVanStichel <jensen.vanstichel@hotmail.com>
2018-12-03 14:19:51 +0100

    gui updates, namespace errors

---
+++ CityApp/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using CityApp.Model;
+using CityApp.ViewModel;
+using CommonServiceLocator;
+
+namespace MvvmLightProbeersel.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class ViewModelLocator
+    {
+        public const string SecondPageKey = "SecondPage";
+
+        /// <summary>
+        /// This property can be used to force the application to run with design time data.
+        /// </summary>
+        public static bool UseDesignTimeData
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            var nav = new NavigationService();
+            nav.Configure(SecondPageKey, typeof(SecondPage));
+            SimpleIoc.Default.Register<INavigationService>(() => nav);
+
+            SimpleIoc.Default.Register<IDialogService, DialogService>();
+
+            if (GalaSoft.MvvmLight.ViewModelBase.IsInDesignModeStatic
+                    || UseDesignTimeData)
+            {
+                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            }
+            else
+            {
+                SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainPageViewModel Main => ServiceLocator.Current.GetInstance<MainPageViewModel>();
+    }
+}
